50fa1312844f60a91bd8e9b0b9f2e9cc04d53053
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2023-12-19 00:41:23 +0100

    Implement basic navigation between pages : pushing routes

--- src/components/AppLayout/Header.tsx
+++ src/components/AppLayout/Header.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import BrandLogo from "../../assets/todo-brand-logo.png";
-import UserImg from "../../assets/user.png";
-
 interface HeaderProps {
     className?: string;
 }
 
 const Header: React.FC<HeaderProps> = (props): React.ReactElement => {
     const { className = undefined } = props;
+
+    const history = useHistory();
+
+    const handleOnLogOut = (): void => {
+        history.push("/login");
+    };
 
     return (
         <header className={className}>
@@ -20,19 +24,23 @@ const Header: React.FC<HeaderProps> = (props): React.ReactElement => {
                 <div className="navbar-brand">
                     <Link to="/home" className="navbar-item">
                         <img src={BrandLogo} alt="Logo" />
-                        <span className="mx-2 has-text-weight-bold is-size-5">
-                            My TODO App
-                        </span>
+                        <span className="mx-2">My TODO App</span>
                     </Link>
                 </div>
                 <div className="navbar-end">
                     <div className="navbar-item has-dropdown is-hoverable is-boxed">
                         <a className="navbar-link is-arrowless">
-                            <img src={UserImg} alt="Logo" />
-                            <span className="mx-2">Profile</span>
+                            <i className="fa fa-user" aria-hidden="true"></i>
+                            <span className="mx-2">--User--</span>
                         </a>
                         <div className="navbar-dropdown is-right">
-                            <a className="navbar-item">Log out</a>
+                            <a
+                                className="navbar-item"
+                                role="button"
+                                onClick={handleOnLogOut}
+                            >
+                                Log out
+                            </a>
                         </div>
                     </div>
                 </div>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,53 +1,65 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
+import BrandLogo from "../assets/todo-brand-logo.png";
 
 const Login: React.FC = (): React.ReactElement => {
+    const history = useHistory();
+
+    const handleOnLogin = (): void => {
+        history.push("/home");
+    };
+
     return (
-        <section className="hero is-primary is-fullheight">
+        <section className="hero is-fullheight">
             <div className="hero-body">
                 <div className="container">
                     <div className="columns is-centered">
                         <div className="column is-5-tablet is-4-desktop is-3-widescreen">
                             <form action="" className="box">
+                                <div className="is-flex is-flex-direction-column m-4">
+                                    <img
+                                        src={BrandLogo}
+                                        alt="Logo"
+                                        className="is-align-self-center m-2"
+                                    />
+                                    <span>My TODO App</span>
+                                </div>
                                 <div className="field">
-                                    <label htmlFor="" className="label">
-                                        Email
-                                    </label>
                                     <div className="control has-icons-left">
                                         <input
-                                            type="email"
-                                            placeholder="e.g. bobsmith@gmail.com"
+                                            type="text"
+                                            placeholder="User"
                                             className="input"
-                                            required
                                         />
                                         <span className="icon is-small is-left">
-                                            <i className="fa fa-envelope"></i>
+                                            <i
+                                                className="fa fa-user"
+                                                aria-hidden="true"
+                                            ></i>
                                         </span>
                                     </div>
                                 </div>
                                 <div className="field">
-                                    <label htmlFor="" className="label">
-                                        Password
-                                    </label>
                                     <div className="control has-icons-left">
                                         <input
                                             type="password"
-                                            placeholder="*******"
+                                            placeholder="Password"
                                             className="input"
-                                            required
                                         />
                                         <span className="icon is-small is-left">
-                                            <i className="fa fa-lock"></i>
+                                            <i
+                                                className="fa fa-lock"
+                                                aria-hidden="true"
+                                            ></i>
                                         </span>
                                     </div>
                                 </div>
                                 <div className="field">
-                                    <label htmlFor="" className="checkbox">
-                                        <input type="checkbox" />
-                                        Remember me
-                                    </label>
-                                </div>
-                                <div className="field">
-                                    <button className="button is-success">
+                                    <button
+                                        className="button is-transparent"
+                                        onClick={handleOnLogin}
+                                    >
                                         Login
                                     </button>
                                 </div>
