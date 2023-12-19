ea8fea10a34a8220ccfca921170335ebd3ceaf19
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2023-12-19 15:15:23 +0100

    Move main styles to a dedicated folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./reset.css";
+import "./styles/reset.css";
 import "bulma/css/bulma.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
